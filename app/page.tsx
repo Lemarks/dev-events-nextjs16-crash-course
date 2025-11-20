c30dd78ea42c17d033a8cf8a56100df106cffa9b
tishmarker <tishmarker@gmail.com>
2025-11-20 12:14:46 -0500

    fix bug with event details

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import ExplorerBtn from "@/components/ExplorerBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
-import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    // const response = await fetch(`${BASE_URL}/api/events`)
-    // const {events} = await response.json()
+    const response = await fetch(`${BASE_URL}/api/events`)
+    const {events} = await response.json()
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
@@ -22,13 +21,11 @@ const Page = async () => {
                 <h3>Featured Events</h3>
 
                 <ul className="events">
-                    {events && events.length > 0 && events.map((event: Pick<IEvent, 'title' | 'image' | 'slug' | 'location' | 'date' | 'time'>) => {
-                        return (
-                            <li key={event.title} className="list-none">
-                                <EventCard {...event}/>
-                            </li>
-                        );
-                    })}
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event}/>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </section>
